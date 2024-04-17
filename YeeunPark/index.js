c2d35380b8cf4ba1224e17253b75cce2cbb53a42
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-04-17 21:53:56 +0900

    Feat: js done

---
+++ YeeunPark/index.js
@@ -0,0 +1,19 @@
+const selectedNum = document.querySelector(".selected_num");
+const result = document.querySelector(".result");
+
+document.querySelectorAll(".btn").forEach(button => {
+    button.addEventListener("click", function() {
+
+        const val = button.value;
+
+        if (val === "=") {
+            const calResult = eval(selectedNum.innerHTML); //calResult에 selectedNum 문자열 값을 넣어 eval 함수로 계산 후 저장
+            result.innerHTML = calResult;
+        } else if (val === "AC") {
+            selectedNum.innerHTML = '';
+            result.innerHTML = '';
+        } else { //입력된 버튼 값을 selectedNum(화면)에 표시
+            selectedNum.innerHTML = selectedNum.innerHTML + val;
+        }
+    });
+})
