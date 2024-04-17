2221b93b1e251959a4e99207a3da408cd9fe4230
yeeun park <145003970+yengniws@users.noreply.github.com>
2024-04-17 22:20:50 +0900

    Feat: 매직넘버 제거

--- YeeunPark/index.js
+++ YeeunPark/index.js
@@ -1,19 +1,21 @@
 const selectedNum = document.querySelector(".selected_num");
 const result = document.querySelector(".result");
+const equal = "=";
+const allClear = "AC";
 
 document.querySelectorAll(".btn").forEach(button => {
     button.addEventListener("click", function() {
 
         const val = button.value;
 
-        if (val === "=") {
+        if (val === equal) {
             try {
                 let calResult = eval(selectedNum.innerHTML); //calResult에 selectedNum 문자열 값을 넣어 eval 함수로 계산 후 저장
                 result.innerHTML = calResult;
             } catch (error) {
-                result.innerHTML = "eval 함수 에러입니다.";
+                result.innerHTML = "유효한 계산식을 입력하세요";
             }
-        } else if (val === "AC") {
+        } else if (val === allClear) {
             selectedNum.innerHTML = '';
             result.innerHTML = '';
         } else { //입력된 버튼 값을 selectedNum(화면)에 표시
